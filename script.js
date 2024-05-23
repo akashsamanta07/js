69849c78a77c0e7796171743b9143b23ef278c44
akashsamanta07 <as5740061@gmail.com>
2024-05-23 21:35:24 +0530

    practice

---
+++ script.js
@@ -0,0 +1,64 @@
+// console.log("AKASH SAMANTA");
+// let num=prompt("Enter your name");
+// console.log(num);
+// let k= 5+ "5";
+// console.log(k);
+// let num1 = prompt("Enter a number:");
+// if(num1 % 5 ===0){
+//     console.log(num1," is multiple by 5");
+// }
+// else{
+//     console.log(num1 ," is not multiple by 5");
+// }
+// let num1=prompt("Enter a number:");
+
+// if(num1 < 40 && num1 >=0){
+//     console.log(num1, " -> F");
+// }
+// else if(num1 >=40 && num1 <50){
+//     console.log(num1, " -> D");
+// }
+// else if(num1 >=50 && num1 <60){
+//     console.log(num1, " -> C");
+// }
+// else if(num1 >=60 && num1 < 70){
+//     console.log(num1, " -> B");
+// }
+// else if(num1 >=70 && num1 < 80){
+//     console.log(num1, " -> A");
+// }
+// else if(num1 >=80 && num1 < 90){
+//     console.log(num1, " -> E");
+// }
+// else if(num1 >=90 && num1 < 100){
+//     console.log(num1, " -> B");
+// }
+// else{
+//     console.log("Out of range number");
+// }
+
+let num2=Number(prompt("Enter a number for day:"));
+switch(num2){
+    case 1: console.log( num2, " -> Monday");
+    break;
+    case 2: console.log( num2, " -> Tuesday");
+    break;
+    case 3: console.log( num2, " -> wednesday");
+    break;
+    case 4: console.log( num2, " -> Thursday");
+    break;
+    case 5: console.log( num2, " -> Friday");
+    break;
+    case 6: console.log( num2, " -> Suturday");
+    break;
+    case 7: console.log( num2, " -> Sunday");
+    break;
+    default: console.log("Out of range");
+    console.log(num2);
+    
+}
+// let num1 = prompt("Enter a number:");
+// let i= num1 %5 ===0 ? console.log(num1," is multiple by 5"):console.log(num1," is not multiple by 5");
+
+
+// console.log(7/6);
